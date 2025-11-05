7e5e38cddb9654531b27fb88012cafd1aea4c81d
Zorrito85 <manuelantonio.carrascofernand1@usp.ceu.es>
2025-11-05 19:16:17 +0100

    VAmoooos

---
+++ cuadros.py/main.py
@@ -0,0 +1,8 @@
+from gioconda_fake import Gioconda_fake
+from gioconda_original import Gioconda
+
+print('Obras de arte')
+print('----------------')
+print(Gioconda)
+print('----------------')
+print(Gioconda_fake)
